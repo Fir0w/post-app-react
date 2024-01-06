307d2a7cee5e794507a3c95a04cef370713f8173
firow <funnym95@gmail.com>
2024-01-06 14:14:53 +0200

    small fix

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, 'client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')));
 } else {
