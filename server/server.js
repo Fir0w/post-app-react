e97c9d2ad9b8697de65ee8014b8962a78e6901c0
firow <funnym95@gmail.com>
2024-01-07 12:13:30 +0200

    fixed path

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html')));
 } else {
     app.get('/', (req, res) => res.send('server is ready'));
 };
