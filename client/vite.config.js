5426a9d943e2368080ee3f5e06a0f9e4a9daecca
Firow <77929714+Fir0w@users.noreply.github.com>
2024-03-25 11:47:07 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://post-app-react-z5ki-mgdy21utf-fir0ws-projects.vercel.app',
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://post-app-react-z5ki-mgdy21utf-fir0ws-projects.vercel.app',
         changeOrigin: true
       }
     }
