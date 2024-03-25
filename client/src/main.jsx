5e3a8c287541067e955f0fc6732a62ba6dc52683
Firow <funnym95@gmail.com>
2024-03-25 14:42:50 +0200

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import ReactDOM from 'react-dom/client'
-import { AuthContextProvider } from './components/AuthContextProvider';
-import axios from 'axios';
-import App from './App'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { AuthContextProvider } from "./components/AuthContextProvider";
+import axios from "axios";
+import App from "./App";
 
-axios.defaults.baseURL = 'https://post-app-react-z5ki-mgdy21utf-fir0ws-projects.vercel.app';
+axios.defaults.baseURL = "https://post-app-react-z5ki.vercel.app";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<AuthContextProvider>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</AuthContextProvider>
-	</React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
+  </React.StrictMode>
 );
