3d89b842852085a628df8790e0b17d53602bf345
Firow <77929714+Fir0w@users.noreply.github.com>
2024-03-25 22:21:31 +0200

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (res, userId) => {
         httpOnly: true,
         // secure: process.env.NODE_ENV !== 'development',
         // sameSite: 'strict',
-        sameSite: 'none',
+        sameSite: 'None',
         maxAge: 30 * 24 * 60 * 60 * 1000
     });
 };
