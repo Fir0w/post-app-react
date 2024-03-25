fbbe422c932d5b80ef313e99113a886cbda005d8
Firow <77929714+Fir0w@users.noreply.github.com>
2024-03-25 22:04:21 +0200

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -9,7 +9,8 @@ const generateToken = (res, userId) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         // secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
+        // sameSite: 'strict',
+        sameSite: 'None',
         domain: "post-app-react-black.vercel.app",
         maxAge: 30 * 24 * 60 * 60 * 1000
     });
