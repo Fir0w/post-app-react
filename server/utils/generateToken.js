822afc6914ae412119fc8442f395dc615aa314f1
Firow <77929714+Fir0w@users.noreply.github.com>
2024-05-08 10:54:52 +0300

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -9,8 +9,7 @@ const generateToken = (res, userId) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
-        // sameSite: 'strict',
-        sameSite: 'None',
+        sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000
     });
 };
