d789009bf093a2e19616336e02b3e9de977ecd20
Firow <77929714+Fir0w@users.noreply.github.com>
2024-05-15 17:01:01 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://post-app-react-z5ki.vercel.app/',
-        changeOrigin: true,
-        secure: true
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://post-app-react-z5ki.vercel.app/',
+  //       changeOrigin: true,
+  //       secure: true
+  //     }
+  //   }
+  // }
 });
